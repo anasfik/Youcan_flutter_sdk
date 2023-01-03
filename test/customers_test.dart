db49d1a444370cdb535aa64c0586f94130ac0662
Gwhyyy <ffikhi.aanas@gmail.com>
2023-01-03 14:53:12 +0100

    [ Add, Edit ] added docs of the create method, edited the name of files

--- test/customers_test.dart
+++ test/customers_test.dart
@@ -1,6 +1,7 @@
 import 'package:test/scaffolding.dart';
 import 'package:test/test.dart';
 import 'package:youcan_flutter_sdk/src/store_front/core/models/auth/customer_creation_response.dart';
+import 'package:youcan_flutter_sdk/src/store_front/core/models/auth/optional_user_informations.dart';
 import 'package:youcan_flutter_sdk/youcan_flutter_sdk.dart';
 
 import 'constants.dart';
@@ -16,6 +17,14 @@ void main() {
         email: email,
         confirmPassword: email,
         password: email,
+        optionalUserInformations: OptionalUserInformations(
+          firstName: 'FIRST NAME',
+          lastName: 'LAST NAME',
+          city: 'CITY',
+          country: 'COUNTRY',
+          notes: 'NOTES',
+          region: 'REGION',
+        ),
       );
 
       expect(createResponse, isA<CustomerCreationResponse>());
