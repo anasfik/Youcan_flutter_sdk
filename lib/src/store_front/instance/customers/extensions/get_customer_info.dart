662bea8b00fe72866fce67f15d3a515a1e3f2722
Gwhyyy <ffikhi.aanas@gmail.com>
2023-01-06 23:10:33 +0100

    [ Add ] added documentation of the customerAccount()

---
+++ lib/src/store_front/instance/customers/extensions/get_customer_info.dart
@@ -0,0 +1,7 @@
+import 'package:youcan_flutter_sdk/src/store_front/instance/customers/customers.dart';
+
+extension GetCustomerInfo on Customers {
+  Future<Map<String, dynamic>> getCustomerInfo() async {
+    
+  }
+}
