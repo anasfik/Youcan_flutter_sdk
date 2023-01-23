cb17067d4879f790bcd4381d9ff3e4780b5e4d0e
Gwhyyy <ffikhi.aanas@gmail.com>
2023-01-23 13:38:01 +0100

    [ Add ] adde documentation comment on the htpl tags extension method, added bool parameter to the experimental logger to enable logging

--- lib/src/store_front/core/utils/html_tags_remove.dart
+++ lib/src/store_front/core/utils/html_tags_remove.dart
@@ -1,4 +1,6 @@
-extension RemoveHtmlTags on String {
+extension RemoveHtmlTagsExtension on String {
+  /// In case of a data us returned as HTML format ( could be from the Rest API ).
+  /// Then you can use this methods to remove all HTML tags, and extract the plain text to be used.
   String removeHtmlTags() {
     return this.replaceAll(RegExp(r"<[^>]*>"), '');
   }
--- lib/src/store_front/core/utils/logger.dart
+++ lib/src/store_front/core/utils/logger.dart
@@ -1,8 +1,17 @@
 import 'dart:developer' as dev;
 
-void customLog(Object message, [String name = 'YouCan']) {
-  dev.log(
-    message.toString(),
-    name: name,
-  );
+import 'package:meta/meta.dart';
+
+/// A simple wrapper around the `dart:developer` log function, to be used as a logger for the SDK.
+/// This is not implemented in use inside the SDK (yet).
+@experimental
+void customLog(
+  Object message,
+  bool enableLogging, [
+  String name = 'YouCan',
+]) {
+  if (enableLogging) {
+    dev.log(message.toString(), name: name);
+  }
+  ;
 }
